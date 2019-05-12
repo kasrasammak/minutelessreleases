6a98e358da2b657e8f1605099bbaec2314eb1305
Kasra Sammak <kasra.sammak@mail.mcgill.ca>
2019-05-12 16:02:27 +0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>MINUTELESS RECORDINGS</h1>
+       <div>
+        <img src="/min2.jpg"></img>
+       </div>
+       <br></br>
+       <a href='./min2.jpg' download>Download Full Album</a>
+       <div>
+        <ol> 
+          <li>doob - Itt</li>
+          <li>Rysavy - Crush</li>
+          <li>V O I D L I F E - 🅕🅡🅐🅖🅜🅔🅝🅣🅢</li>
+          <li>Tenebral Cortex - Lost Between Dimensions</li>
+          <li>Enigmachina - Unintention</li>
+          <li>K-Owl - Mixed Up People</li>
+          <li>Audionimus - Farmer Seeds</li>
+          <li>Nox - Waking Down</li>
+          <li>Fractal Terrorist - o-[qp]-o</li>
+          <li>Gobbo - Nmetum</li>
+          <li>Voj - Ab Anit</li>
+          <li>Mirror Me - no minutes just chillin'</li>
+          <li>Audiosyntax - Scorpios Dilemma</li>
+          <li>Beepetc. - Remind</li>
+        </ol>
+      <div>
+        <p class="description">
+          This is the first release of Minuteless Recordings. <br></br>
+          Compiled by Kasra Sammak of K-Owl. <br></br>
+          It is a collection of tracks from 14 different artists. <br></br>
+          The intent behind this compilation was<br></br>
+          to create something new and interesting, <br></br>
+          and we hope we have accomplished that. <br></br>
+          All the production behind this compilation was done in-house: <br></br>
+          Tracks written and produced by artists themselves.<br></br>
+          Mastering done by Daniel of Audionimus<br></br>
+          with the exemption of tracks x, y, and z, <br></br>
+          which were self-masters.<br></br>
+          Artwork done by Kasra Sammak.<br></br>
+          Artwork is a blend of hand-drawings made by various artists from this compilation.<br></br>
+          No other images were used to make this artwork. <br></br>
+          Even this website itself was written by Kasra Sammak.<br></br>
+          You may not preview this compilation, you can only download it for free.<br></br>
+          We hope you enjoy!!!
+        </p>
+        <br></br>
+      </div>
+      </div>
+       
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
