777ed804cb0781af75f21c669485f07f228edcfc
Kasra Sammak <kasra.sammak@mail.mcgill.ca>
2019-05-24 20:10:08 +0200

    added banner

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>MINUTELESS RECORDINGS</h1>
+      <br></br>
+      {/* <h1>MINUTELESS RECORDINGS</h1> */}
+      <img src="/banner.jpg"></img>
        <div>
+         <br></br><br></br>
         <img src="/min5.jpg"></img>
        </div>
        <br></br>
