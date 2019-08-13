2713b575c7e4905677982ccbc14d02cfe97efee7
Kasra Sammak <kasra.sammak@mail.mcgill.ca>
2019-08-14 00:20:23 +0200

    somechanges

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <img class="albumart"src="/min5.jpg"></img>
        </div>
        <br></br>
-       <a href='https://www.dropbox.com/s/hayxb6e5p1exmhc/minutelessvamaster.zip?dl=0' download>Download Full Album</a>
+       <a href='https://drive.google.com/drive/folders/1oJjItBypWsJUCULm_YsKeVI_P-9pMPwc?usp=sharing' download>Download Full Album</a>
        <div>
         <ol> 
           <li>doob - Itt</li>
@@ -37,24 +37,25 @@ function App() {
         <p class="description">
           This is the first release of Minuteless Recordings. <br></br>
           Compiled by Kasra Sammak of K-Owl. <br></br>
-          It is a collection of tracks from 14 different artists. <br></br>
+          It is a collection of tracks from 15 different artists. <br></br>
           The intent behind this compilation was<br></br>
-          to create something new and interesting, <br></br>
-          and we are happy even if we've only made a small stride towards that goal. <br></br>
+          to create something new and interesting. <br></br>
+          Who knows how close we were. <br></br>
           All the production behind this compilation was done in-house: <br></br>
           Tracks written and produced by artists themselves.<br></br>
-          Mastering done by Ioannis Karapostolis of Tenebral Cortex<br></br>
-          with the exemption of tracks Voj - Ab Anit, and Romeodark - 🅕🅡🅐🅖🅜🅔🅝🅣🅢 <br></br>
+          Mastering by Ioannis Karapostolis of Tenebral Cortex<br></br>
+          with the exemption of tracks  03 Romeodark - 🅕🅡🅐🅖🅜🅔🅝🅣🅢 and 12 Voj - Ab Anit <br></br>
           which were self-mastered.<br></br>
           Artwork by Kasra Sammak.<br></br>
           Artwork is a blend of hand-drawings made by various artists from this compilation.<br></br>
           No other images were used to make this artwork. <br></br>
           Even this website itself was written by Kasra Sammak.<br></br>
-          You may not preview this compilation, you can only download it for free.<br></br>
+          You may not currently preview this compilation,<br></br>
+          you may only download it for free.<br></br>
           We hope you enjoy!<br></br><br></br>
          
-          Also, if you liked our tunes don't forget to follow our <a href="https://www.soundcloud.com/minutelessrecordings">Soundcloud,</a>
-          where we are continuously posting music from our artists,<br></br> as well as other artists who inspire us!
+          Follow our <a href="https://www.soundcloud.com/minutelessrecordings">Soundcloud,</a>
+          where we post music from our artists,<br></br> as well as others who inspire us!
           <br></br><br></br>
           Love,<br></br>
           Minuteless Team
